fb935e8e836feff337b8c6a6137a7d654eb65808
linderkm <kevin.linder94@gmail.com>
2024-12-11 20:58:19 -0700

    adding db schema and db diagram png

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,45 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+
+
+CREATE TABLE category (
+    category_id VARCHAR(30) PRIMARY KEY  NOT NULL,
+    category VARCHAR(30)   NOT NULL
+);
+
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR(30) PRIMARY KEY  NOT NULL,
+    subcategory VARCHAR(30)   NOT NULL
+);
+
+CREATE TABLE contacts (
+    contact_id int PRIMARY KEY  NOT NULL,
+    first_name VARCHAR(30)   NOT NULL,
+    last_name VARCHAR(30)   NOT NULL,
+    email VARCHAR(60)   NOT NULL
+
+);
+
+
+-- https://www.postgresql.org/docs/current/datatype-datetime.html
+CREATE TABLE campgaigns (
+    cf_id int PRIMARY KEY  NOT NULL,
+    contact_id int   NOT NULL,
+	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+    company_name VARCHAR(30)   NOT NULL,
+    description VARCHAR(30)   NOT NULL,
+    goal real   NOT NULL,
+    pledged real   NOT NULL,
+    outcome VARCHAR(30)   NOT NULL,
+    backers_count int   NOT NULL,
+    country VARCHAR(30)   NOT NULL,
+    currency VARCHAR(30)   NOT NULL,
+    launch_date timestamp   NOT NULL,
+    end_date timestamp   NOT NULL,
+    category_id VARCHAR(30)   NOT NULL,
+	FOREIGN KEY (category_id) REFERENCES category(category_id),
+    subcategory_id VARCHAR(30)   NOT NULL,
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+
+);
+
+
